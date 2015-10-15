4bf5bb9797ab5ab91ac2ecfde98ca9b8ec8d78a9
Axel  Rosén <parosen@student.matnat.uio.no>
2015-10-15 13:31:36 +0200

    skrivit statement, statmlist, emptystatm, while

---
+++ no/uio/ifi/pascal2100/parser/EmptyStatm.java
@@ -0,0 +1,41 @@
+package no.uio.ifi.pascal2100.parser;
+
+import no.uio.ifi.pascal2100.main.*;
+import no.uio.ifi.pascal2100.scanner.*;
+
+class EmptyStatm extends Statement {
+    EmptyStatm(int lNum) {
+    super(lNum);
+    }
+
+    
+    @Override public String identify() {
+    return "<empty statm> on line " + lineNum;
+    }
+
+
+    @Override void check(Block curScope, Library lib) {
+    // Til del 3 av prosjektet
+    }
+
+
+    @Override void genCode(CodeFile f) {
+    // Til del 4 av prosjektet
+    }
+
+
+    @Override void prettyPrint() {
+    // Til neste ukes oppgaver
+    }
+
+
+    static EmptyStatm parse(Scanner s) {
+    enterParser("empty statm"); 
+
+    EmptyStatm ess = new EmptyStatm(s.curLineNum());
+    // Fyll ut resten her.
+
+    leaveParser("empty statm");
+    return ess;
+    }
+}
---
+++ no/uio/ifi/pascal2100/parser/StatmList.java
@@ -0,0 +1,47 @@
+
+package no.uio.ifi.pascal2100.parser;
+
+import no.uio.ifi.pascal2100.main.*;
+import no.uio.ifi.pascal2100.scanner.*;
+import static no.uio.ifi.pascal2100.scanner.TokenKind.*;
+
+import java.util.ArrayList;
+
+class StatmList extends PascalSyntax {
+    ArrayList<Statement> statements = new ArrayList<Statement>();
+
+    StatmList(int lNum) {
+        super(lNum);
+    }
+
+
+    @Override public String identify() {
+        return "<statm list> on line " + lineNum;
+    }
+
+
+    @Override void check(Block curScope, Library lib) {
+        // Til del 3 av prosjektet
+    }
+
+
+    @Override void genCode(CodeFile f) {
+        // Til del 4 av prosjektet
+    }
+
+
+    @Override void prettyPrint() {
+        // Til neste ukes oppgaver
+    }
+
+
+    static StatmList parse(Scanner s) {
+        enterParser("statm list");
+
+        StatmList sl = new StatmList(s.curLineNum());
+            // Fyll ut resten her.
+
+        leaveParser("statm list");
+        return sl;
+    }
+}
