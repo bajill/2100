3781305e45bb3219bf402d6b646bd287ba1cf248
Kenneth Aune Frisvold <kennetaf@rubin.ifi.uio.no>
2015-10-15 13:51:10 +0200

    Block

---
+++ no/uio/ifi/pascal2100/parser/Block.java
@@ -0,0 +1,9 @@
+package no.uio.ifi.pascal2100.parser;
+import no.uio.ifi.pascal2100.main.*;
+import no.uio.ifi.pascal2100.scanner.*;
+import static no.uio.ifi.pascal2100.scanner.TokenKind.*;
+/* Block ::= <const decl part> <type decl part> <var decl part> 
+    <func decl> || <proc decl> 'begin' <starm list> 'end' */
+public class Block extends PascalDecl{
+
+}
