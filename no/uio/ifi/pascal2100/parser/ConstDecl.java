578d579ea455a59f90237add30fb7b0ee5995b93
Kenneth Aune Frisvold <kennetaf@rubin.ifi.uio.no>
2015-10-17 15:37:54 +0200

    working on block, constDecl files

---
+++ no/uio/ifi/pascal2100/parser/ConstDecl.java
@@ -0,0 +1,33 @@
+
+package no.uio.ifi.pascal2100.parser;
+import no.uio.ifi.pascal2100.main.*;
+import no.uio.ifi.pascal2100.scanner.*;
+import static no.uio.ifi.pascal2100.scanner.TokenKind.*;
+
+public class ConstDecl extends PascalDecl {
+    Constant constant;
+    ConstDecl(String name, int lNum) {
+        super(name, lNum);
+    }
+
+
+    @Override public String identify() {
+        return "<const decl> on line " + lineNum;
+    }
+
+    public static ConstDecl parse(Scanner s) {
+        enterParser("const decl"); 
+        s.test(nameToken);
+
+        /* Gives superclass the string name */
+        ConstDecl cd = new ConstDecl(s.curToken.id, s.curLineNum());
+        s.readNextToken();
+        s.skip(equalToken);
+
+        cd.constant = NumberLiteral.parse(s);      
+        s.skip(semicolonToken);
+
+        leaveParser("const decl");
+        return cd;
+    }
+}
---
+++ no/uio/ifi/pascal2100/parser/NumberLiteral.java
@@ -0,0 +1,27 @@
+
+package no.uio.ifi.pascal2100.parser;
+import no.uio.ifi.pascal2100.main.*;
+import no.uio.ifi.pascal2100.scanner.*;
+import static no.uio.ifi.pascal2100.scanner.TokenKind.*;
+
+class NumberLiteral extends Constant {
+    int numValue;
+    NumberLiteral(int lNum) {
+    super(lNum);
+    }
+
+    
+    @Override public String identify() {
+    return "<numeric literal> on line " + lineNum;
+    }
+
+    static NumberLiteral parse(Scanner s) {
+        enterParser("numeric literal"); 
+        NumberLiteral nl = new NumberLiteral(s.curLineNum());
+        s.test(intValToken);
+        nl.numValue = s.curToken.intVal;
+        s.readNextToken();
+        leaveParser("numeric literal");
+        return nl;
+    }
+}
