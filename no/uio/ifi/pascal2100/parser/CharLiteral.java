53e750c55fffae85b561f772a16e25ec0ac9187f
Kenneth Aune Frisvold <kennetaf@rubin.ifi.uio.no>
2015-10-20 12:23:13 +0200

    added files

---
+++ no/uio/ifi/pascal2100/parser/CharLiteral.java
@@ -0,0 +1,30 @@
+
+package no.uio.ifi.pascal2100.parser;
+import no.uio.ifi.pascal2100.main.*;
+import no.uio.ifi.pascal2100.scanner.*;
+import static no.uio.ifi.pascal2100.scanner.TokenKind.*;
+
+class CharLiteral extends Constant{
+    String name;
+    CharLiteral(String name, int lNum) {
+        super(lNum);
+        this.name = name;
+    }
+
+
+    @Override public String identify() {
+        return "<char literal> on line " + lineNum;
+    }
+
+    @Override public void prettyPrint() {
+
+    }
+
+    static CharLiteral parse(Scanner s) {
+        enterParser("char literal"); 
+        CharLiteral cl = new CharLiteral(s.curToken.id, s.curLineNum());
+        s.readNextToken();
+        leaveParser("char literal");
+        return cl;
+    }
+}
