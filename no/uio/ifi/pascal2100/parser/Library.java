f131ca4aa88dceece733d2bc2ee088f4310ccb1b
Kenneth Aune Frisvold <kennetaf@rubin.ifi.uio.no>
2015-11-02 19:10:34 +0100

    empty lib

--- no/uio/ifi/pascal2100/parser/Library.java
+++ no/uio/ifi/pascal2100/parser/Library.java
@@ -6,25 +6,10 @@ import static no.uio.ifi.pascal2100.scanner.TokenKind.*;
 import java.util.ArrayList;
 
 class Library extends Block{
-    ArrayList<String> predefDecls;
+    HashMap<String, PascalDecl> decls = new HashMap<String, PascalDecl>();
 
     Library() {
         super(0);
-        predefDecls = new ArrayList<String>();
 
-        predefDecls.add("write");
-        predefDecls.add("true");
-        predefDecls.add("false");
-        predefDecls.add("eol");
-    }
-
-    @Override void check(Block curScope, Library lib){
-        String id = "write";
-        // If its not write, true, false or eol, check for integer and char and
-        // log error of not
-        if(!predefDecls.contains(id)){
-            System.out.println("Library: int or char");
-        }
-        
     }
 }
