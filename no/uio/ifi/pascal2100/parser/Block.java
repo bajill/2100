a13145d0bb16653cde088a961ce31d16235966a7
Axel  Rosén <parosen@student.matnat.uio.no>
2015-12-01 14:47:37 +0100

    ;ldkfjalsdfjk

--- no/uio/ifi/pascal2100/parser/Block.java
+++ no/uio/ifi/pascal2100/parser/Block.java
@@ -35,25 +35,27 @@ class Block extends PascalSyntax{
 
 
     public void genCode(CodeFile f){
-        for (ProcDecl p: procANDfuncDecl) {
-            System.out.println("procDecl: " +p.progProcFuncname);
-        }
-
         for(ProcDecl pd: procANDfuncDecl)
             pd.genCode(f);
 
         if(blockLevel == 1)
             f.genInstr("prog$" + name+ "_" + blockLevel, "", "", "");
-        else
-            f.genInstr("proc$" + name + "_" + blockLevel,
-                    "", "", "");
-
+        else {
+            if(outerScope.findDecl(name, this) instanceof FuncDecl) {
+                f.genInstr("proc$" + name + "_" + blockLevel,"", "", "");
+                f.genInstr("", "enter", "$" +(32 + 4*(offSet)) + ", $" + blockLevel,
+                        "Start of name??"); 
+                statmList.genCode(f);
+                f.genInstr("", "movl", "-32(%ebp),%eax", "Fetch return value");
+                f.genInstr("", "leave", "", "");
+                f.genInstr("", "ret", "", "");
+                return;                
+            }
+            f.genInstr("proc$" + name + "_" + blockLevel,"", "", "");
+        }
         f.genInstr("", "enter", "$" +(32 + 4*(offSet)) + ", $" + blockLevel,
                 "Start of name??"); 
         statmList.genCode(f);
-        //if (this is a function) 
-        if (false)
-            f.genInstr("", "movl", "-32(%ebp),%eax", "Fetch return value");
         f.genInstr("", "leave", "", "");
         f.genInstr("", "ret", "", "");
 
